e651f5b64b4b1a00099a0bb7bc855abcff528d4e
testmessenger <testrendermeds@gmail.com>
2026-01-18 01:04:29 +0500

    Update main.py

--- my-messenger/main.py
+++ my-messenger/main.py
@@ -6,35 +6,46 @@ import time
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit, join_room
 from pymongo import MongoClient
+from bson import ObjectId
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'pro-messenger-2026'
 socketio = SocketIO(app, cors_allowed_origins="*", max_http_buffer_size=1000 * 1024 * 1024)
 
-# ПОДКЛЮЧЕНИЕ К БАЗЕ ДАННЫХ
-# Замени ЭТУ ССЫЛКУ на свою из MongoDB Atlas!
-MONGO_URL = "mongodb+srv://admin:ТВОЙ_ПАРОЛЬ@cluster0.abcde.mongodb.net/?retryWrites=true&w=majority"
+# ПОДКЛЮЧЕНИЕ К ТВОЕЙ БАЗЕ
+MONGO_URL = "mongodb+srv://adminbase:admin123@cluster0.iw8h40a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 client = MongoClient(MONGO_URL)
 db = client['messenger_db']
 
-# Коллекции (таблицы) в базе
 users_col = db['users']
 rooms_col = db['rooms']
 messages_col = db['messages']
 
-# Создаем общий чат по умолчанию, если его нет
+# Создаем общий чат при первом запуске
 if not rooms_col.find_one({"name": "Общий чат"}):
-    rooms_col.insert_one({
-        "name": "Общий чат", 
-        "type": "group", 
-        "owner": "system", 
-        "admins": [], 
-        "members": []
-    })
+    rooms_col.insert_one({"name": "Общий чат", "type": "group", "owner": "system", "admins": [], "members": []})
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@socketio.on('login_attempt')
+def handle_login(data):
+    nick = data['nick']
+    password = data['pass']
+    name = data['name']
+    
+    user = users_col.find_one({"nick": nick})
+    
+    if user:
+        if user['password'] == password:
+            emit('login_success', {"name": user['name'], "nick": nick})
+        else:
+            emit('login_error', "Неверный пароль для этого @username")
+    else:
+        # Регистрация нового пользователя
+        users_col.insert_one({"nick": nick, "password": password, "name": name})
+        emit('login_success', {"name": name, "nick": nick})
 
 @socketio.on('join')
 def on_join(data):
@@ -42,62 +53,51 @@ def on_join(data):
     nick = data['nick']
     join_room(room_name)
     
-    # Добавляем участника в базу чата
-    rooms_col.update_one(
-        {"name": room_name},
-        {"$addToSet": {"members": nick}}
-    )
+    rooms_col.update_one({"name": room_name}, {"$addToSet": {"members": nick}})
     
-    # Загружаем историю из базы
+    # Загрузка истории
     history = list(messages_col.find({"room": room_name}).sort("_id", -1).limit(50))
-    for m in history: m['_id'] = str(m['_id']) # Исправляем формат ID для JSON
+    formatted_history = []
+    for m in history:
+        formatted_history.append({"user": m['user'], "nick": m['nick'], "text": m['text']})
     
-    emit('history', history[::-1])
+    emit('history', formatted_history[::-1])
     
-    # Рассылаем инфо о комнате из базы
     room_data = rooms_col.find_one({"name": room_name})
-    room_data['_id'] = str(room_data['_id'])
-    emit('room_info', room_data, to=room_name)
+    if room_data:
+        room_data['_id'] = str(room_data['_id'])
+        emit('room_info', room_data, to=room_name)
 
 @socketio.on('message')
 def handle_message(data):
-    data['timestamp'] = time.time()
-    # Сохраняем сообщение в базу навсегда
-    messages_col.insert_one(data.copy())
-    
-    # Отправляем в чат (удаляем системный ID базы перед отправкой)
-    if '_id' in data: del data['_id']
-    emit('render_message', data, to=data.get('room'))
+    messages_col.insert_one({
+        "room": data['room'],
+        "user": data['user'],
+        "nick": data['nick'],
+        "text": data['text'],
+        "time": time.time()
+    })
+    emit('render_message', data, to=data['room'])
 
 @socketio.on('create_room')
 def create_room(data):
-    name = data['name']
-    if not rooms_col.find_one({"name": name}):
-        new_room = {
-            "name": name,
+    if not rooms_col.find_one({"name": data['name']}):
+        rooms_col.insert_one({
+            "name": data['name'],
             "type": data['type'],
             "owner": data['user'],
             "admins": [],
             "members": [data['user']]
-        }
-        rooms_col.insert_one(new_room)
-        emit('room_created', {"name": name}, broadcast=True)
+        })
+        emit('room_created', {"name": data['name']}, broadcast=True)
 
 @socketio.on('make_admin')
 def make_admin(data):
     room = data['room']
-    target = data['target']
-    requester = data['requester']
-    
-    room_data = rooms_col.find_one({"name": room})
-    if room_data and room_data['owner'] == requester:
-        rooms_col.update_one(
-            {"name": room},
-            {"$addToSet": {"admins": target}}
-        )
-        updated_room = rooms_col.find_one({"name": room})
-        updated_room['_id'] = str(updated_room['_id'])
-        emit('room_info', updated_room, to=room)
+    rooms_col.update_one({"name": room, "owner": data['requester']}, {"$addToSet": {"admins": data['target']}})
+    updated = rooms_col.find_one({"name": room})
+    updated['_id'] = str(updated['_id'])
+    emit('room_info', updated, to=room)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))

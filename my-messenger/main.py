c6703276419c15584455f567e9dd0225ef163851
testmessenger <testrendermeds@gmail.com>
2026-01-18 02:46:00 +0500

    Update main.py

--- my-messenger/main.py
+++ my-messenger/main.py
@@ -8,10 +8,9 @@ from pymongo import MongoClient
 from bson import ObjectId
 
 app = Flask(__name__)
-# Поддержка больших данных (голосовые)
-socketio = SocketIO(app, cors_allowed_origins="*", max_http_buffer_size=100*1024*1024)
+# Увеличиваем лимит для передачи фото
+socketio = SocketIO(app, cors_allowed_origins="*", max_http_buffer_size=10 * 1024 * 1024)
 
-# Подключение к базе (connect=False убирает KeyError)
 MONGO_URL = "mongodb+srv://adminbase:admin123@cluster0.iw8h40a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tlsAllowInvalidCertificates=true"
 client = MongoClient(MONGO_URL, connect=False)
 db = client['messenger_db']
@@ -24,37 +23,37 @@ def index(): return render_template('index.html')
 def login(data):
     nick = data['nick'].replace('@', '').lower()
     user = users_col.find_one({"nick": nick})
-    if user and user['password'] == data['pass']:
-        emit('login_success', {"name": user['name'], "nick": nick, "avatar": user.get('avatar', '')})
-    elif not user:
-        users_col.insert_one({"nick": nick, "password": data['pass'], "name": data['name'], "avatar": ""})
+    if user:
+        if user['password'] == data['pass']:
+            emit('login_success', {"name": user['name'], "nick": nick, "avatar": user.get('avatar', '')})
+        else: emit('login_error', "Неверный пароль!")
+    else:
+        new_user = {"nick": nick, "password": data['pass'], "name": data['name'], "avatar": ""}
+        users_col.insert_one(new_user)
         emit('login_success', {"name": data['name'], "nick": nick, "avatar": ""})
-    else: emit('login_error', "Ошибка!")
+
+@socketio.on('update_profile_image')
+def update_img(data):
+    users_col.update_one({"nick": data['nick']}, {"$set": {"avatar": data['img']}})
+    emit('avatar_updated', data['img'])
+
+@socketio.on('search')
+def search(data):
+    q = data['query'].lower()
+    users = list(users_col.find({"nick": {"$regex": q}}, {"_id":0, "password":0}))
+    rooms = list(rooms_col.find({"name": {"$regex": q}}, {"_id":0}))
+    emit('search_results', {"users": users, "rooms": rooms})
 
 @socketio.on('message')
 def handle_msg(data):
-    data['time'] = time.time()
-    res = messages_col.insert_one(data.copy())
+    res = messages_col.insert_one(data)
     data['_id'] = str(res.inserted_id)
     emit('render_message', data, to=data['room'])
-
-# СИГНАЛЫ ДЛЯ ЗВОНКОВ
-@socketio.on('call_user')
-def handle_call(data):
-    emit('calling', data, room=data['room'], include_self=False)
-
-@socketio.on('answer_call')
-def handle_answer(data):
-    emit('call_accepted', data, room=data['room'], include_self=False)
-
-@socketio.on('ice_candidate')
-def handle_ice(data):
-    emit('remote_ice', data, room=data['room'], include_self=False)
 
 @socketio.on('join')
 def on_join(data):
     join_room(data['room'])
-    h = list(messages_col.find({"room": data['room']}).sort("_id", -1).limit(50))
+    h = list(messages_col.find({"room": data['room']}).sort("_id", -1).limit(40))
     for m in h: m['_id'] = str(m['_id'])
     emit('history', h[::-1])
 
@@ -64,7 +63,6 @@ def get_rooms():
 
 @socketio.on('create_room')
 def create_r(data):
-    data['admins'] = [data['creator']]
     rooms_col.update_one({"id": data['id']}, {"$set": data}, upsert=True)
     emit('room_created', data, broadcast=True)
 

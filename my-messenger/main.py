7e7ae70ce744d7dadd1dc45ad41b0504b776404d
testmessenger <testrendermeds@gmail.com>
2026-01-18 01:11:43 +0500

    Update main.py

--- my-messenger/main.py
+++ my-messenger/main.py
@@ -9,10 +9,10 @@ from pymongo import MongoClient
 from bson import ObjectId
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'pro-messenger-2026-secure'
-socketio = SocketIO(app, cors_allowed_origins="*", max_http_buffer_size=1000 * 1024 * 1024)
+app.config['SECRET_KEY'] = 'secure-key-2026'
+# Увеличиваем лимит для передачи картинок (до 20МБ)
+socketio = SocketIO(app, cors_allowed_origins="*", max_http_buffer_size=20 * 1024 * 1024)
 
-# ПОДКЛЮЧЕНИЕ К БАЗЕ
 MONGO_URL = "mongodb+srv://adminbase:admin123@cluster0.iw8h40a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 client = MongoClient(MONGO_URL)
 db = client['messenger_db']
@@ -43,15 +43,13 @@ def on_join(data):
     room_name = data['room']
     nick = data['nick']
     join_room(room_name)
-    rooms_col.update_one({"name": room_name}, {"$addToSet": {"members": nick}})
     
-    # Загрузка истории (превращаем ObjectId в строку для JS)
+    # Загрузка истории
     history = list(messages_col.find({"room": room_name}).sort("_id", -1).limit(50))
-    for m in history:
-        m['_id'] = str(m['_id'])
-    
+    for m in history: m['_id'] = str(m['_id'])
     emit('history', history[::-1])
     
+    # Если это группа, обновляем инфо
     room_data = rooms_col.find_one({"name": room_name})
     if room_data:
         room_data['_id'] = str(room_data['_id'])
@@ -59,46 +57,38 @@ def on_join(data):
 
 @socketio.on('message')
 def handle_message(data):
-    # Сохраняем и получаем ID
+    # data может содержать 'file' и 'file_type'
     msg_obj = {
         "room": data['room'],
         "user": data['user'],
         "nick": data['nick'],
-        "text": data['text'],
+        "text": data.get('text', ''),
+        "file": data.get('file'), 
+        "file_type": data.get('file_type'),
         "time": time.time()
     }
     result = messages_col.insert_one(msg_obj)
     data['_id'] = str(result.inserted_id)
     emit('render_message', data, to=data['room'])
 
+@socketio.on('search_user')
+def search_user(data):
+    query = data['query'].replace('@', '').lower()
+    user = users_col.find_one({"nick": query})
+    if user:
+        emit('user_found', {"nick": user['nick'], "name": user['name']})
+    else:
+        emit('login_error', "Пользователь не найден")
+
 @socketio.on('edit_message')
 def edit_message(data):
-    msg_id = data['msg_id']
-    new_text = data['new_text']
-    messages_col.update_one({"_id": ObjectId(msg_id)}, {"$set": {"text": new_text}})
-    emit('message_edited', {"msg_id": msg_id, "new_text": new_text}, to=data['room'])
+    messages_col.update_one({"_id": ObjectId(data['msg_id'])}, {"$set": {"text": data['new_text']}})
+    emit('message_edited', data, to=data['room'])
 
 @socketio.on('delete_message')
 def delete_message(data):
     messages_col.delete_one({"_id": ObjectId(data['msg_id'])})
-    emit('message_deleted', {"msg_id": data['msg_id']}, to=data['room'])
-
-@socketio.on('delete_chat')
-def delete_chat(data):
-    room = rooms_col.find_one({"name": data['room']})
-    if room and room['owner'] == data['requester']:
-        rooms_col.delete_one({"name": data['room']})
-        messages_col.delete_many({"room": data['room']})
-        emit('chat_deleted', {"room": data['room']}, broadcast=True)
-
-@socketio.on('create_room')
-def create_room(data):
-    if not rooms_col.find_one({"name": data['name']}):
-        rooms_col.insert_one({
-            "name": data['name'], "type": "group", 
-            "owner": data['user'], "admins": [], "members": [data['user']]
-        })
-        emit('room_created', {"name": data['name']}, broadcast=True)
+    emit('message_deleted', data, to=data['room'])
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))

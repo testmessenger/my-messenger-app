7521b688968c830eb398cf9ed2d3d83cc925d797
testmessenger <testrendermeds@gmail.com>
2026-01-18 03:22:45 +0500

    Update main.py

--- my-messenger/main.py
+++ my-messenger/main.py
@@ -1,14 +1,15 @@
 from gevent import monkey
 monkey.patch_all()
 
-import os, time
+import os
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit, join_room, disconnect
+from flask_socketio import SocketIO, emit, join_room
 from pymongo import MongoClient
 
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*", max_http_buffer_size=20 * 1024 * 1024)
+socketio = SocketIO(app, cors_allowed_origins="*", max_http_buffer_size=50 * 1024 * 1024)
 
+# Подключение к БД
 MONGO_URL = "mongodb+srv://adminbase:admin123@cluster0.iw8h40a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tlsAllowInvalidCertificates=true"
 client = MongoClient(MONGO_URL, connect=False)
 db = client['messenger_db']
@@ -20,50 +21,43 @@ def index(): return render_template('index.html')
 @socketio.on('login_attempt')
 def login(data):
     nick = data['nick'].replace('@', '').lower().strip()
-    password = data['pass'].strip()
     user = users_col.find_one({"nick": nick})
-    
     if user:
-        if user.get('banned'):
-            emit('login_error', "Ваш аккаунт заблокирован!")
-            return
-        if user['password'] == password:
-            emit('login_success', {"name": user['name'], "nick": nick, "avatar": user.get('avatar', ''), "rank": user.get('rank', 'Участник')})
+        if user.get('banned'): return emit('login_error', "Вы забанены!")
+        if user['password'] == data['pass']:
+            emit('login_success', {"name": user['name'], "nick": nick, "avatar": user.get('avatar', ''), "rank": user.get('rank', 'Участник'), "bio": user.get('bio', '')})
         else: emit('login_error', "Неверный пароль!")
     else:
         rank = "Админ" if users_col.count_documents({}) == 0 else "Участник"
-        users_col.insert_one({"nick": nick, "password": password, "name": data.get('name') or nick, "avatar": "", "rank": rank, "banned": False})
-        emit('login_success', {"name": data.get('name') or nick, "nick": nick, "avatar": "", "rank": rank})
+        new_u = {"nick": nick, "password": data['pass'], "name": data.get('name') or nick, "avatar": "", "rank": rank, "bio": "", "banned": False}
+        users_col.insert_one(new_u)
+        emit('login_success', {"name": new_u['name'], "nick": nick, "avatar": "", "rank": rank, "bio": ""})
 
-@socketio.on('get_members')
-def get_members(data):
-    members = []
-    for u in users_col.find({"banned": {"$ne": True}}):
-        members.append({"nick": u['nick'], "name": u['name'], "rank": u.get('rank', 'Участник'), "avatar": u.get('avatar', '')})
-    emit('members_list', members)
+@socketio.on('get_my_rooms')
+def get_my_rooms(data):
+    nick = data['nick']
+    # Показываем комнаты, где пользователь в списке members
+    my_rooms = list(rooms_col.find({"members": nick}, {"_id": 0}))
+    emit('load_rooms', my_rooms)
 
-@socketio.on('change_rank')
-def change_rank(data):
-    # data: { target_nick, new_rank, admin_nick }
-    admin = users_col.find_one({"nick": data['admin_nick']})
-    if admin and admin.get('rank') == "Админ":
-        users_col.update_one({"nick": data['target_nick']}, {"$set": {"rank": data['new_rank']}})
-        emit('system_message', f"Ранг @{data['target_nick']} изменен на {data['new_rank']}", broadcast=True)
-        # Перезапрашиваем список участников у всех
-        members = [{"nick": u['nick'], "name": u['name'], "rank": u.get('rank', 'Участник')} for u in users_col.find({"banned": {"$ne": True}})]
-        emit('members_list', members, broadcast=True)
+@socketio.on('search')
+def search(data):
+    q = data['query'].lower().strip()
+    if not q: return
+    users = list(users_col.find({"nick": {"$regex": q}}, {"_id":0, "password":0}))
+    rooms = list(rooms_col.find({"name": {"$regex": q, "$options": "i"}}, {"_id":0}))
+    emit('search_results', {"users": users, "rooms": rooms})
 
-@socketio.on('ban_user')
-def ban_user(data):
-    # data: { target_nick, admin_nick }
-    admin = users_col.find_one({"nick": data['admin_nick']})
-    if admin and admin.get('rank') in ["Админ", "Модератор"]:
-        target = users_col.find_one({"nick": data['target_nick']})
-        if target and target.get('rank') == "Админ": return # Админа нельзя банить
-        
-        users_col.update_one({"nick": data['target_nick']}, {"$set": {"banned": True}})
-        emit('system_message', f"Пользователь @{data['target_nick']} забанен!", broadcast=True)
-        emit('kick_signal', data['target_nick'], broadcast=True)
+@socketio.on('join_room_request')
+def join_r(data):
+    rooms_col.update_one({"id": data['room_id']}, {"$addToSet": {"members": data['nick']}})
+    emit('load_rooms_trigger')
+
+@socketio.on('create_room')
+def create_r(data):
+    data['members'] = [data['creator']]
+    rooms_col.insert_one(data)
+    emit('room_created', data)
 
 @socketio.on('message')
 def handle_msg(data):
@@ -77,16 +71,32 @@ def handle_msg(data):
 @socketio.on('join')
 def on_join(data):
     join_room(data['room'])
-    h = []
-    for m in messages_col.find({"room": data['room']}).sort("_id", -1).limit(50):
-        m.pop('_id', None)
-        h.append(m)
+    h = [ {k:v for k,v in m.items() if k != '_id'} for m in messages_col.find({"room": data['room']}).sort("_id", -1).limit(50) ]
     emit('history', h[::-1])
 
-@socketio.on('get_rooms')
-def get_rooms():
-    rooms = [ {k:v for k,v in r.items() if k != '_id'} for r in rooms_col.find({}) ]
-    emit('load_rooms', rooms)
+@socketio.on('get_members')
+def get_members(data=None): # data=None исправляет твою ошибку!
+    if not data or 'room' not in data:
+        # Если комната не указана, просто шлем список всех для ЛС
+        m_list = list(users_col.find({"banned": {"$ne": True}}, {"_id":0, "password":0}).limit(30))
+    else:
+        room = rooms_col.find_one({"id": data['room']})
+        if room and 'members' in room:
+            m_list = list(users_col.find({"nick": {"$in": room['members']}}, {"_id":0, "password":0}))
+        else:
+            m_list = list(users_col.find({"banned": {"$ne": True}}, {"_id":0, "password":0}).limit(30))
+    emit('members_list', m_list)
+
+@socketio.on('update_profile')
+def update_profile(data):
+    users_col.update_one({"nick": data['nick']}, {"$set": {"name": data['name'], "bio": data['bio'], "avatar": data['avatar']}})
+
+@socketio.on('ban_user')
+def ban(data):
+    admin = users_col.find_one({"nick": data['admin_nick']})
+    if admin and admin.get('rank') in ["Админ", "Модератор"]:
+        users_col.update_one({"nick": data['target_nick']}, {"$set": {"banned": True}})
+        emit('kick_signal', data['target_nick'], broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    socketio.run(app, host='0.0.0.0', port=5000)

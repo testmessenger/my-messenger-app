68a4dcd4811e649bb2ab7bdf425dfe3149dd45b8
testmessenger <testrendermeds@gmail.com>
2026-01-18 04:08:44 +0500

    Update main.py

--- my-messenger/main.py
+++ my-messenger/main.py
@@ -6,8 +6,9 @@ from flask_socketio import SocketIO, emit, join_room
 from pymongo import MongoClient
 
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*", max_http_buffer_size=50 * 1024 * 1024)
+socketio = SocketIO(app, cors_allowed_origins="*", max_http_buffer_size=100 * 1024 * 1024)
 
+# Подключение к БД
 MONGO_URL = "mongodb+srv://adminbase:admin123@cluster0.iw8h40a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tlsAllowInvalidCertificates=true"
 client = MongoClient(MONGO_URL, connect=False)
 db = client['messenger_db']
@@ -23,7 +24,7 @@ def login(data):
     if user:
         if user['password'] == data['pass']:
             emit('login_success', {"name": user['name'], "nick": nick, "avatar": user.get('avatar', ''), "bio": user.get('bio', '')})
-        else: emit('login_error', "Ошибка")
+        else: emit('login_error', "Ошибка пароля")
     else:
         new_u = {"nick": nick, "password": data['pass'], "name": nick, "avatar": "", "bio": ""}
         users_col.insert_one(new_u)
@@ -31,15 +32,23 @@ def login(data):
 
 @socketio.on('message')
 def handle_msg(data):
-    data['id'] = str(os.urandom(4).hex())
-    data['reactions'] = {} # Инициализация пустых реакций
+    data['id'] = str(os.urandom(6).hex())
+    data['reactions'] = {}
     messages_col.insert_one(data.copy())
     data.pop('_id', None)
     emit('render_message', data, to=data['room'])
 
+@socketio.on('delete_message')
+def delete_msg(data):
+    # data: {msg_id, room, nick}
+    # Проверяем, что удаляет автор (базовая защита)
+    msg = messages_col.find_one({"id": data['msg_id']})
+    if msg and msg['nick'] == data['nick']:
+        messages_col.delete_one({"id": data['msg_id']})
+        emit('remove_message_from_ui', data['msg_id'], to=data['room'])
+
 @socketio.on('add_reaction')
 def add_reaction(data):
-    # data: {msg_id, emoji, nick, room}
     messages_col.update_one({"id": data['msg_id']}, {"$set": {f"reactions.{data['nick']}": data['emoji']}})
     msg = messages_col.find_one({"id": data['msg_id']}, {"_id": 0})
     emit('update_reactions', msg, to=data['room'])
@@ -50,20 +59,16 @@ def create_room(data):
     rooms_col.insert_one({"id": room_id, "name": data['name'], "members": [data['creator_nick']], "type": "group"})
     emit('load_rooms', list(rooms_col.find({"members": data['creator_nick']}, {"_id": 0})))
 
-@socketio.on('join')
-def on_join(data):
-    join_room(data['room'])
-    h = list(messages_col.find({"room": data['room']}).sort("_id", -1).limit(40))
-    for m in h: m.pop('_id', None)
-    emit('history', h[::-1])
-
 @socketio.on('get_my_rooms')
 def get_rooms(data):
     emit('load_rooms', list(rooms_col.find({"members": data['nick']}, {"_id": 0})))
 
-@socketio.on('get_members')
-def get_m(data):
-    emit('members_list', list(users_col.find({}, {"_id":0, "password":0}).limit(50)))
+@socketio.on('join')
+def on_join(data):
+    join_room(data['room'])
+    h = list(messages_col.find({"room": data['room']}).sort("_id", -1).limit(50))
+    for m in h: m.pop('_id', None)
+    emit('history', h[::-1])
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000)

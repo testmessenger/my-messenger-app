9aa4e8dafe40550fa6ac1063a0aeff0662cd6388
testmessenger <testrendermeds@gmail.com>
2026-01-18 02:00:20 +0500

    Update main.py

--- my-messenger/main.py
+++ my-messenger/main.py
@@ -8,7 +8,7 @@ from pymongo import MongoClient
 from bson import ObjectId
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'ultra-litegram-2026'
+app.config['SECRET_KEY'] = 'litegram-ultra-secret'
 socketio = SocketIO(app, cors_allowed_origins="*", max_http_buffer_size=100 * 1024 * 1024)
 
 # Подключение к MongoDB
@@ -17,7 +17,7 @@ client = MongoClient(MONGO_URL)
 db = client['messenger_db']
 users_col = db['users']
 messages_col = db['messages']
-rooms_col = db['rooms'] # Коллекция для групп и каналов
+rooms_col = db['rooms']
 
 @app.route('/')
 def index():
@@ -30,17 +30,21 @@ def handle_login(data):
     if user:
         if user['password'] == data['pass']:
             emit('login_success', {"name": user['name'], "nick": nick, "avatar": user.get('avatar', '')})
-        else: emit('login_error', "Неверный пароль!")
+        else: emit('login_error', "Ошибка!")
     else:
-        new_user = {"nick": nick, "password": data['pass'], "name": data['name'], "avatar": "", "last_seen": time.time()}
+        new_user = {"nick": nick, "password": data['pass'], "name": data['name'], "avatar": ""}
         users_col.insert_one(new_user)
         emit('login_success', {"name": data['name'], "nick": nick, "avatar": ""})
+
+@socketio.on('get_my_rooms')
+def get_rooms():
+    rooms = list(rooms_col.find({}, {"_id": 0}))
+    emit('load_rooms', rooms)
 
 @socketio.on('join')
 def on_join(data):
     room = data['room']
     join_room(room)
-    # Загружаем историю конкретной комнаты
     history = list(messages_col.find({"room": room}).sort("_id", -1).limit(50))
     for m in history: m['_id'] = str(m['_id'])
     emit('history', history[::-1])
@@ -48,17 +52,13 @@ def on_join(data):
 @socketio.on('message')
 def handle_message(data):
     data['time'] = time.time()
-    user = users_col.find_one({"nick": data['nick'].replace('@','')})
-    data['avatar'] = user.get('avatar', '') if user else ''
     res = messages_col.insert_one(data.copy())
     data['_id'] = str(res.inserted_id)
     emit('render_message', data, to=data['room'])
 
 @socketio.on('create_room')
 def create_room(data):
-    # data = {name, type, creator}
-    room_id = data['name'].lower().replace(' ', '_')
-    rooms_col.update_one({"id": room_id}, {"$set": data}, upsert=True)
+    rooms_col.update_one({"id": data['id']}, {"$set": data}, upsert=True)
     emit('room_created', data, broadcast=True)
 
 @socketio.on('typing')
@@ -70,5 +70,4 @@ def update_avatar(data):
     users_col.update_one({"nick": data['nick']}, {"$set": {"avatar": data['avatar']}})
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port)
+    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))

643e341ed805b9c19a6d1874fd4e0d8812933bd8
testmessenger <testrendermeds@gmail.com>
2026-01-18 13:55:54 +0500

    Update main.py

--- my-messenger/main.py
+++ my-messenger/main.py
@@ -7,11 +7,11 @@ import base64
 from flask import Flask, render_template, request, session, redirect, url_for, jsonify
 from flask_socketio import SocketIO, emit, join_room
 from pymongo import MongoClient
-from werkzeug.security import generate_password_hash, check_password_hash
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'nexus_elite_2026'
+app.config['SECRET_KEY'] = 'nexus_ultra_call_system_2026'
 
 MONGO_URI = "mongodb+srv://adminbase:admin123@cluster0.iw8h40a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tlsAllowInvalidCertificates=true"
 client = MongoClient(MONGO_URI)
@@ -33,7 +33,7 @@ def index():
 @app.route('/auth')
 def auth(): return render_template('auth.html')
 
-# --- API: ГРУППЫ И СПИСОК ЧАТОВ ---
+# --- API (БЕЗ ВЫРЕЗАНИЯ) ---
 @app.route('/api/my_chats')
 def get_my_chats():
     user = get_user()
@@ -41,91 +41,65 @@ def get_my_chats():
     for g in groups: g['_id'] = str(g['_id'])
     return jsonify(groups)
 
-@app.route('/api/groups/create', methods=['POST'])
-def create_group():
-    user = get_user()
-    gid = db.groups.insert_one({
-        "title": request.json['title'],
-        "owner_id": str(user['_id']),
-        "members": [str(user['_id'])],
-        "admins": [str(user['_id'])],
-        "muted": [], "banned": []
-    }).inserted_id
-    return jsonify({"id": str(gid)})
-
-# --- API: УПРАВЛЕНИЕ УЧАСТНИКАМИ ---
-@app.route('/api/group/manage', methods=['POST'])
-def manage_member():
-    user = get_user()
-    data = request.json # {room_id, target_id, action}
-    group = db.groups.find_one({"_id": ObjectId(data['room_id'])})
-    
-    if str(user['_id']) != group['owner_id'] and str(user['_id']) not in group['admins']:
-        return "No power", 403
-
-    if data['action'] == 'make_admin':
-        db.groups.update_one({"_id": group['_id']}, {"$addToSet": {"admins": data['target_id']}})
-    elif data['action'] == 'kick':
-        db.groups.update_one({"_id": group['_id']}, {"$pull": {"members": data['target_id'], "admins": data['target_id']}})
-    elif data['action'] == 'mute':
-        db.groups.update_one({"_id": group['_id']}, {"$addToSet": {"muted": data['target_id']}})
-    
-    return jsonify({"status": "ok"})
-
-# --- API: ПРОФИЛЬ И ЗАГРУЗКА ---
-@app.route('/api/profile/save', methods=['POST'])
-def save_profile():
-    user = get_user()
-    data = request.json
-    upd = {"display_name": data['name'], "bio": data['bio']}
-    if data.get('avatar'): upd["avatar"] = data.get('avatar')
-    db.users.update_one({"_id": user['_id']}, {"$set": upd})
-    return jsonify({"status": "ok"})
-
 @app.route('/api/upload', methods=['POST'])
 def upload():
     file = request.files['file']
     encoded = base64.b64encode(file.read()).decode('utf-8')
     return jsonify({"url": f"data:{file.content_type};base64,{encoded}"})
 
-@app.route('/api/user/<user_id>')
-def get_user_info(user_id):
-    u = db.users.find_one({"_id": ObjectId(user_id)}, {"password": 0})
-    u['_id'] = str(u['_id'])
-    return jsonify(u)
+@app.route('/api/room/<room_id>/members')
+def get_members(room_id):
+    if room_id == 'general':
+        users = list(db.users.find({}, {"password":0}).limit(50))
+    elif "_" in room_id:
+        ids = [ObjectId(x) for x in room_id.split("_")]
+        users = list(db.users.find({"_id": {"$in": ids}}, {"password":0}))
+    else:
+        group = db.groups.find_one({"_id": ObjectId(room_id)})
+        users = list(db.users.find({"_id": {"$in": [ObjectId(m) for m in group['members']]}}, {"password":0}))
+    for u in users: u['_id'] = str(u['_id'])
+    return jsonify(users)
 
-# --- SOCKET.IO ---
+# --- SOCKET.IO (ЗВОНКИ И УВЕДОМЛЕНИЯ) ---
 @socketio.on('join_room')
 def on_join(data):
     join_room(data['room'])
-    msgs = list(db.messages.find({"room": data['room']}).sort("ts", -1).limit(40))
-    for m in reversed(msgs):
-        m['_id'] = str(m['_id'])
-        emit('new_message', m)
 
 @socketio.on('send_msg')
 def handle_msg(data):
     user = get_user()
-    msg_obj = {
+    msg = {
         "room": data['room'], "sender_id": str(user['_id']),
         "sender_name": user['display_name'], "sender_avatar": user['avatar'],
         "text": data.get('text', ''), "type": data.get('type', 'text'),
-        "file_url": data.get('file_url', ''), "ts": datetime.datetime.utcnow().isoformat(),
-        "reactions": {}
+        "file_url": data.get('file_url', ''), "ts": datetime.datetime.utcnow().isoformat()
     }
-    res = db.messages.insert_one(msg_obj)
-    msg_obj['_id'] = str(res.inserted_id)
-    emit('new_message', msg_obj, room=data['room'])
+    db.messages.insert_one(msg)
+    emit('new_message', msg, room=data['room'])
+    # Уведомление для других
+    emit('notify', {"title": f"Новое от {user['display_name']}", "body": data.get('text', 'Файл/Кружок')}, room=data['room'], include_self=False)
 
-@socketio.on('add_reaction')
-def react(data):
-    db.messages.update_one({"_id": ObjectId(data['msg_id'])}, {"$set": {f"reactions.{data['emoji']}": True}})
-    emit('reaction_update', data, room=data['room'])
+# WebRTC Сигналинг
+@socketio.on('call_user')
+def call_user(data):
+    emit('incoming_call', {
+        "from": data['from'],
+        "caller_name": data['caller_name'],
+        "room": data['room'],
+        "offer": data['offer']
+    }, room=data['room'], include_self=False)
 
-@socketio.on('delete_msg')
-def delete(data):
-    db.messages.delete_one({"_id": ObjectId(data['msg_id'])})
-    emit('msg_deleted', data['msg_id'])
+@socketio.on('answer_call')
+def answer_call(data):
+    emit('call_accepted', {"answer": data['answer']}, room=data['room'])
+
+@socketio.on('ice_candidate')
+def ice_candidate(data):
+    emit('ice_candidate', data['candidate'], room=data['room'], include_self=False)
+
+@socketio.on('hangup')
+def hangup(data):
+    emit('call_ended', room=data['room'])
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=10000)
